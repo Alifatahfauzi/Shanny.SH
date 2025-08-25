101af5f96a1014778ac3f330cde22ecd814d3a09
Fauzialifatah <118640580+Alifatahfauzi@users.noreply.github.com>
2025-08-25 19:25:15 +0900

    #-Shanny

---
+++ command/shanny.js
@@ -0,0 +1,9 @@
+let handler = async (m, { reply }) => {
+    reply("> Shanny.SH x Fauzialifatah");
+};
+
+handler.help = ['yes'];
+handler.tags = ['main'];
+handler.command = ["yes"];
+
+module.exports = handler;
